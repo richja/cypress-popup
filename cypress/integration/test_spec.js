46a56d46e812f65dfc459fd382fb6018a39d37fa
Jan Richter <jan.richter@alchemyglobalsolutions.com>
2021-03-30 22:12:23 +0100

    use before load event to cover all page transitions

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -1,10 +1,11 @@
 describe('testing popup function', () => {
     it('navigates to index and stubs window.open', () => {
-        cy.visit('index.html', {
-            onBeforeLoad: (win) => {
-                cy.stub(win, 'open');
-            }
-        });
+        cy.on('window:before:load', (win) => {
+            cy.stub(win, 'open')
+            console.log('page transition')
+        })
+
+        cy.visit('index.html');
 
         cy.window().then(function (win) {
             cy.get('button').click()
